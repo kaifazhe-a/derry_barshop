918ea5ab9a4da370cc26a5ce234bfcaf6635cfc6
Shanghh <shh18749927259@163.com>
2020-03-06 17:24:36 +0800

    init commit

---
+++ src/main/java/com/dj/ssm/DerryBarshopApplication.java
@@ -0,0 +1,13 @@
+package com.dj.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerryBarshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DerryBarshopApplication.class, args);
+    }
+
+}
