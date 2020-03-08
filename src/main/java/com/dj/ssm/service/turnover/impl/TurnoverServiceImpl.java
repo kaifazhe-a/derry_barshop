5b91f06d077e1d38d5f3b60a7bb7cd942c55901a
csg <15036313593@163.com>
2020-03-08 15:45:21 +0800

    营业额api  后台

---
+++ src/main/java/com/dj/ssm/service/turnover/impl/TurnoverServiceImpl.java
@@ -0,0 +1,18 @@
+package com.dj.ssm.service.turnover.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.dj.ssm.mapper.turnover.TurnoverMapper;
+import com.dj.ssm.pojo.TurnoverPojo;
+import com.dj.ssm.service.turnover.TurnoverService;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * 营业额api实现类
+ */
+@Service
+public class TurnoverServiceImpl extends ServiceImpl<TurnoverMapper, TurnoverPojo> implements TurnoverService {
+
+
+}
